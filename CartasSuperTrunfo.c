428b03d5b7193c8dde7552e89bb63eecc05e1634
Tainara Paula <147462577+Tainaraoficial@users.noreply.github.com>
2025-08-31 21:39:43 +0000

    novo commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,6 +9,7 @@
 int main() {
 
     printf("Desafio Super Trunfo!\n");
+    printf("Olá, mundo");
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
